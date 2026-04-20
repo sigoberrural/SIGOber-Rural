5bd12e5c70cc827818d937292064e958ce090c58
sigoberrural <sigoberrural@gmail.com>
2026-04-20 23:55:44 +0200

    Create main.py
    Hola Mundo

---
+++ app/main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("SIGOber-Rural: Puerto Rico (Caquetá)")
+st.subheader("Sistema de Gobernanza Rural y Seguimiento al CMDR")
+
+st.write("Bienvenido Jhon Fredy. El software está listo para recibir datos de la Fase 1.")
