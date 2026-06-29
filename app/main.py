93382b446ea8377dfca6f9b6fe33590eff1c1bed
sigoberrural <sigoberrural@gmail.com>
2026-06-29 13:20:26 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -138,7 +138,7 @@ AUTOGENERADO_HTML = """<!DOCTYPE html>
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
     <script src="https://layerjs.org/libs/turf.min.js"></script>
     <script>
-        const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzthjqZZasXvEbjIneaGdHUy3waVNmMs99fbKwANg08cj_0mVF2FPC7ly7vmOR_WWMiew/exec";
+        const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwNKhNoX_Tq6IvOKP26jLuyIsyrfYuRnQum-5FDVhi6mHECriPiN65zC5tdrIXK7-nRgQ/exec";
         let mapa, marcador, capaVeredas;
         const LAT_DEFECTO = 1.9123; const LON_DEFECTO = -75.1842;
         const datosVeredasGeoJSON = {"type": "FeatureCollection", "features": [{"type": "Feature", "properties": {"NOMBRE_VER": "Zona Rural General - Puerto Rico"}, "geometry": {"type": "Polygon", "coordinates": [[[-75.30, 2.05], [-75.00, 2.05], [-75.00, 1.80], [-75.30, 1.80], [-75.30, 2.05]]]}}]};
